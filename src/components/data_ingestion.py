10eaf7559a534484de5f33a90b9b4a3bf28fe71d
Karntapong <bladesephiroth01@gmail.com>
2024-03-08 01:05:51 +0700

    transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -31,7 +31,8 @@ class DataIngestion:
             logging.info('Ingestion completed')
             return (
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path
+                self.ingestion_config.test_data_path,
+                self.ingestion_config.raw_data_path
             )
         except Exception as e:
             raise CustomerException(e,sys)
---
+++ src/components/data_tranformation.py
@@ -0,0 +1,69 @@
+import sys
+from dataclasses import dataclass
+
+import numpy as np 
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+from sklearn.model_selection import train_test_split
+from src.exception import CustomException
+from src.logger import logging
+import os
+from src.utils import save_object
+
+
+
+@dataclass
+class DataTransformationconfig:
+    preprocessor_obj_file_path = os.path.join('artifacts','preprocessor.pkl')
+
+
+class DataTransformation:
+    def __init__(self):
+        self.data_tranformation_config == DataTransformationconfig()
+
+
+    def initiate_data_transformation(self,data_path):
+        try:
+            df = pd.read_csv(data_path)
+            num_features = df.select_dtypes(exclude='object').columns
+            df_model = df.copy()
+            df_model['average_score'] = df[num_features].sum(axis=1)/len(num_features)
+            select_columns = ['math_score','reading_score','writing_score']
+            df_model.drop(select_columns,axis=1,inplace=True)
+            X = df_model.drop(columns=['average_score'],axis = 1)
+            y = df_model['average_score']
+            num_features = X.select_dtypes(exclude="object").columns
+            cat_features = X.select_dtypes(include="object").columns
+            numeric_transformer = StandardScaler()
+            oh_transformer = OneHotEncoder(sparse=False)
+            preprocessor = ColumnTransformer(
+                [
+                    ("OneHotEncoder", oh_transformer, cat_features),
+                    ("StandardScaler", numeric_transformer, num_features),        
+                ]
+            )
+
+            # Fit and transform the data
+            X_encoded = preprocessor.fit_transform(X)
+
+            # Fit the OneHotEncoder
+            oh_transformer.fit(X[cat_features])
+
+            # Get column names for one-hot encoded features
+            encoded_column_names = oh_transformer.get_feature_names_out(cat_features)
+
+            # Combine column names of one-hot encoded and numerical features
+            final_column_names = list(encoded_column_names) + list(X.select_dtypes(exclude="object").columns)
+
+            # Convert the transformed array to a DataFrame with column names
+            X_encoded_df = pd.DataFrame(X_encoded, columns=final_column_names)
+
+            X_train, X_test, y_train, y_test = train_test_split(X_encoded_df,y,test_size=0.2,random_state=42)
+
+            return X_train,X_test,y_train,y_test
+
+        except Exception as e:
+            raise CustomException(e,sys)
