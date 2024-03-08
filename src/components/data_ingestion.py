4e1d72aad74633e3226ca6d05ce1008b1920df38
Karntapong <bladesephiroth01@gmail.com>
2024-03-09 01:19:08 +0700

    kuy

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -39,6 +39,7 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e,sys)
+
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data,df=obj.initiate_data_ingestion()
